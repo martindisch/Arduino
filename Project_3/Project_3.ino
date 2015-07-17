aa812554ed3e307bc50acaa9be3e83eca7f9ca90
Martin Disch <martindisch@gmail.com>
2015-07-17 09:48:53 +0200

    Displaying temperature with LEDs

--- Project_3/Project_3.ino
+++ Project_3/Project_3.ino
@@ -1,5 +1,8 @@
 const int sensorPin = A0;
-const float baselineTemp = 20.0;
+const float baselineTemp = 24.0;
+
+int sensorVal, lightAmount;
+float voltage, temperature;
 
 void setup() {
   Serial.begin(9600);
@@ -10,13 +13,43 @@ void setup() {
 }
 
 void loop() {
-  int sensorVal = analogRead(sensorPin);
+  sensorVal = analogRead(sensorPin);
   Serial.print("Sensor value: ");
   Serial.print(sensorVal);
-  float voltage = (sensorVal/1024.0) * 5.0;
+  voltage = (sensorVal/1024.0) * 5.0;
   Serial.print(", Volts: ");
   Serial.print(voltage);
   Serial.print(", degrees C: ");
-  float temperature = (voltage - .5) * 100;
+  temperature = (voltage - .5) * 100;
   Serial.println(temperature);
+
+  lightAmount = 0;
+  if (temperature >= baselineTemp + 2) {
+    lightAmount = 1;
+    if (temperature >= baselineTemp + 4) {
+      lightAmount = 2;
+      if (temperature >= baselineTemp + 6) {
+        lightAmount = 3;
+      }
+    }
+  }
+  light(lightAmount);
+
+  delay(500);
 }
+
+void light(int amount) {
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  digitalWrite(4, LOW);
+  if (amount >= 1) {
+    digitalWrite(2, HIGH);
+    if (amount >= 2) {
+      digitalWrite(3, HIGH);
+      if (amount >= 3) {
+        digitalWrite(4, HIGH);
+      }
+    }
+  }
+}
+
