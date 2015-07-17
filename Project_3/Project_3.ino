241e6d7a1a9fddfc6dd2b0b42069acb5991bd7da
Martin Disch <martindisch@gmail.com>
2015-07-17 18:19:23 +0200

    Extended project 3 to save temperatures and dump on button press

--- Project_3/Project_3.ino
+++ Project_3/Project_3.ino
@@ -1,55 +1,86 @@
 const int sensorPin = A0;
-const float baselineTemp = 24.0;
+const float baselineTemp = 23.0;
+const float interval = 5 * 60000;
+const int waitDuration = 250;
 
 int sensorVal, lightAmount;
 float voltage, temperature;
+float temperatures[300];
+long loopCounter = 0;
+int storeIndex = 0;
 
 void setup() {
   Serial.begin(9600);
-  for (int i = 2; i < 5; i++) {
+  for (int i = 2; i < 8; i++) {
     pinMode(i, OUTPUT);
-    digitalWrite(i, LOW);
   }
+  pinMode(8, INPUT);
 }
 
 void loop() {
   sensorVal = analogRead(sensorPin);
-  Serial.print("Sensor value: ");
-  Serial.print(sensorVal);
   voltage = (sensorVal/1024.0) * 5.0;
-  Serial.print(", Volts: ");
-  Serial.print(voltage);
-  Serial.print(", degrees C: ");
   temperature = (voltage - .5) * 100;
-  Serial.println(temperature);
 
   lightAmount = 0;
-  if (temperature >= baselineTemp + 2) {
-    lightAmount = 1;
-    if (temperature >= baselineTemp + 4) {
-      lightAmount = 2;
-      if (temperature >= baselineTemp + 6) {
-        lightAmount = 3;
-      }
+  for (int i = 1; i < 7; i++) {
+    if (temperature >= baselineTemp + i) {
+      lightAmount = i;
+    }
+    else {
+      break;
     }
   }
   light(lightAmount);
 
-  delay(500);
+  if (loopCounter % (int) (interval / waitDuration) == 0) {
+    store(temperature);
+  }
+
+  input();
+
+  loopCounter++;
+  delay(waitDuration);
 }
 
 void light(int amount) {
   digitalWrite(2, LOW);
   digitalWrite(3, LOW);
   digitalWrite(4, LOW);
+  digitalWrite(5, LOW);
+  digitalWrite(6, LOW);
+  digitalWrite(7, LOW);
   if (amount >= 1) {
     digitalWrite(2, HIGH);
     if (amount >= 2) {
       digitalWrite(3, HIGH);
       if (amount >= 3) {
         digitalWrite(4, HIGH);
+        if (amount >= 4) {
+          digitalWrite(5, HIGH);
+          if (amount >= 5) {
+            digitalWrite(6, HIGH);
+            if (amount >= 6) {
+              digitalWrite(7, HIGH);
+            }
+          }
+        }
       }
     }
   }
+}
+
+void input() {
+  if (digitalRead(8) == HIGH) {
+    for (int i = 0; i < storeIndex; i++) {
+      Serial.println(temperatures[i]);
+    }
+    storeIndex = 0;
+  }
+}
+
+void store(float temperature) {
+  temperatures[storeIndex] = temperature;
+  storeIndex++;
 }
 
