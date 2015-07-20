9dcfc84968d081aef8547b998742e492962d0c17
Martin Disch <martindisch@gmail.com>
2015-07-20 10:59:19 +0200

    Reading whole lines from Arduino

--- Serial/Serial.ino
+++ Serial/Serial.ino
@@ -1,8 +1,15 @@
+int incomingByte = 0;
+const int ledPin = 2;
+int counter = 0;
+
 void setup() {
   Serial.begin(9600);
+  pinMode(2, OUTPUT);
 }
 
 void loop() {
-  Serial.println("Hello world");
-  delay(10);
+  Serial.print("I'm at count ");
+  Serial.println(counter);
+  counter++;
+  delay(100);
 }
--- Serial/reader.py
+++ Serial/reader.py
@@ -1,11 +1,21 @@
 import serial
 import time
 ser = serial.Serial('COM3', 9600, timeout=0)
- 
+
+def receiving(ser):
+    global last_received
+    buffer_string = ''
+    while True:
+        buffer_string = buffer_string + ser.read(ser.inWaiting())
+        if '\n' in buffer_string:
+            lines = buffer_string.split('\n') # Guaranteed to have at least 2 entries
+            last_received = lines[-2]
+            #If the Arduino sends lots of empty lines, you'll lose the
+            #last filled line, so you could make the above statement conditional
+            #like so: if lines[-2]: last_received = lines[-2]
+            buffer_string = lines[-1]
+            return last_received
+
 while 1:
-    try:
-        print ser.readline()
-        time.sleep(1)
-    except ser.SerialTimeoutException:
-        print('Data could not be read')
-        time.sleep(1)
+    print receiving(ser)
+    
