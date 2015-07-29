a1e4266442ae59d42af6b5b3174d4f3b0132949d
Martin Disch <martindisch@gmail.com>
2015-07-29 10:55:04 +0200

    Removed LCD

--- Watering/Watering.ino
+++ Watering/Watering.ino
@@ -1,10 +1,4 @@
-#include <LiquidCrystal.h>
-
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-const int contrastPin = 6;
-const int contrast = 40;
-
-const int gate = 9;
+const int gate = 11;
 int mode = 0;
 int outputValue;
 
@@ -12,32 +6,22 @@ void setup() {
   Serial.begin(9600);
   pinMode(gate, OUTPUT);
   Serial.println("Enter 0 or 1");
-
-  lcd.begin(16, 2);
-  pinMode(contrastPin, OUTPUT);
-  analogWrite(contrastPin, contrast);
-  lcd.print("Type 0 or 1");
-  lcd.setCursor(0, 1);
-  lcd.print("in serial");
 }
 
 void loop() {
   if (Serial.available()) {
     mode = Serial.parseInt();
     if (mode == 0) {
+      Serial.println("Stopping motor");
       digitalWrite(gate, LOW);
     }
     else {
+      Serial.println("Starting motor");
       startMotor();
     }
     delay(500);
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("New value ");
-    lcd.print(mode);
-    lcd.setCursor(0, 1);
-    lcd.write("Enter 0 or 1");
   }
+  delay(50);
 }
 
 void startMotor() {
