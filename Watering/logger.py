ee2c7c2d9935ed0173db3fc55ef3c106934da973
Martin Disch <martindisch@gmail.com>
2015-08-03 13:50:00 +0200

    Logging humidity hourly

--- Watering/logger.py
+++ Watering/logger.py
@@ -1,6 +1,7 @@
-import serial, time
+import serial, time, numpy
 
-threshold = 485
+watchTime = 3600
+measureInterval = 5
 
 def receiving(ser):
     global last_received
@@ -14,18 +15,30 @@ def receiving(ser):
             return last_received
 
 def dateTime():
-    return time.strftime("%Y/%m/%d %H:%M:%S  ")
+    return time.strftime("%Y/%m/%d %H:%M:%S")
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
 
 # Don't write immediately, the Arduino is restarting
 time.sleep(3)
 
+timePassed = 0
+values = ()
+startDate = "none"
+
 while 1:
+    if timePassed >= watchTime:
+        print startDate + " - " + dateTime() + "    " + str(numpy.mean(values))        
+        timePassed = 0
+        values = ()
+        startDate = "none"
+
+    if "none" in startDate:
+        startDate = dateTime()
+
     ser.write('4')
     message = receiving(ser)
     value = int(message[-4:])
-    if (value < threshold):
-        print dateTime() + str(value)
-        threshold -= 5        
-    time.sleep(5)
+    values += (value,)
+    timePassed += measureInterval        
+    time.sleep(measureInterval)
